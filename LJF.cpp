d757dd743aaf4b8b1cbb6588f2b67d6472cc5eb8
rajmishra07 <mrrajmishra573@gmail.com>
2023-08-28 21:44:41 +0530

    1)FCFS implimentation with varying arrival times 2)SJF implimentaion with zero arrival time 3)LJF implimentaion with varying arrival time

---
+++ LJF.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct data1{
+    int pid;
+    int at;
+    int bt;
+    int ct;
+    int tat;
+    int wt;
+};
+
+bool cmp(data1 x,data1 y){
+    return (x.bt > y.bt);
+}
+int main(){
+    data1 row[20];
+    int n;
+    float wtavd,tatavg;
+    cout<<"Enter the number of process:";
+    cin>>n;
+    cout<<"Enter arrival time: ";
+    for(int i = 0; i < n; i++){
+        row[i].pid = i;
+         cin>>row[i].at;
+    }
+     cout<<"Enter burst time: ";
+    for(int i = 0; i < n; i++){
+        cin>>row[i].bt;
+        
+    }
+    sort(row,row+n,cmp);
+    
+        row[0].ct = row[0].bt;
+        row[0].tat = row[0].ct-row[0].at;
+        row[0].wt = row[0].tat-row[0].bt;
+        for(int i=0;i<n;i++){
+            row[i].ct = row[i-1].ct+row[i].bt;
+            row[i].tat = row[i].ct - row[i].at;
+            row[i].wt = row[i].tat - row[i].bt;
+        }
+        
+        for(int i = 0;i<n;i++){
+            cout<<row[i].ct<<" "<<row[i].pid<<" "<<row[i].at<<" "<<row[i].wt<<" "<<row[i].tat<<endl;
+        }
+}
